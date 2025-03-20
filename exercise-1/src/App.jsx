75746ee9809e3d9c877a3fa38fc75f6ed21a3b00
lida-sokha <sokhalida1234@gmail.com>
2025-03-20 13:29:54 +0700

    ex-1

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -20,12 +20,12 @@ function App() {
 
   function getBackgroundColor() {
     // This function manage the main class value, depending on the weather
-    return isSunny ? "--sun-color" : "--rain-color";
+    return isSunny ? "sunny" : "rainy";
   }
 
   return (
     <main>
-      <h1>TODO</h1>
+      <h1>{getTitle()}</h1>
       <button onClick ={onSunClick}>Sunny Time</button>
       <button onClick ={onRainClick} >Rain Time</button>
     </main>
